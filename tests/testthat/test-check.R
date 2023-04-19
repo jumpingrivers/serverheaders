19adcc2a22fc03c016d987ef40f6242c6551426e
Mandy Norrbo <mandy@jumpingrivers.com>
2023-04-19 13:34:53 +0100

    Update tests with new dimensions

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,6 +1,6 @@
 test_that("Testing check", {
   x = check("www.google.com")
   expect_equal(x$status_code, 200)
-  expect_equal(ncol(x$headers), 4)
-  expect_gt(nrow(x$headers), 20)
+  expect_equal(ncol(x$headers), 5)
+  expect_gt(nrow(x$headers), 18)
 })
