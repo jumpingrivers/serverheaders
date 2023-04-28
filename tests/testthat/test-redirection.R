1d3c2fccd1c8fe059422d8c10e7251ab7dd27e70
Mandy Norrbo <mandy@jumpingrivers.com>
2023-04-28 08:31:44 +0100

    Update redirection tests

--- tests/testthat/test-redirection.R
+++ tests/testthat/test-redirection.R
@@ -1,14 +1,6 @@
 test_that("Get redirection works", {
-  res = list(
-    all_headers = list(
-    list(
-      "status" = 200,
-      "version" = "HTTP/2"
-      )
-    ))
-  expect_true(get_redirection(res))
-  res$all_headers[[1]][["version"]] = "HTTP/1.1"
+  res = httr::GET("neverssl.com")
   expect_false(get_redirection(res))
-  res$all_headers[[1]][["status"]] = 301
+  res = httr::GET("google.com")
   expect_true(get_redirection(res))
 })
